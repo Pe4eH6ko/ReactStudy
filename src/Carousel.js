74ecd0db9aef13496e756519ff5384fe2781feb8
Ivan Poturnak <Poturnak.Ivan@gmail.com>
2019-04-12 14:29:08 +0300

    make carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,28 +1,41 @@
 import React from 'react';
+import './Carousel.scss';
 
 class Carousel extends React.Component{
     render(){
         return(
-            <div id="carouselExampleControls" className="carousel slide carouselBlock" data-ride="carousel">
+            <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                        <img className="d-block w-100" src={require('./img/Carousel/1.jpg')} alt="First slide"></img>
+                        <img className="d-block w-100 carousel-image" src={require('./img/Carousel/html.jpg')} alt="First slide"></img>
                     </div>
                     <div className="carousel-item">
-                        <img className="d-block w-100" src={require('./img/Carousel/2.jpg')} alt="Second slide"></img>
+                        <img className="d-block w-100 carousel-image" src={require('./img/Carousel/css.png')} alt="Second slide"></img>
                     </div>
                     <div className="carousel-item">
-                        <img className="d-block w-100" src={require('./img/Carousel/3.jpg')} alt="Third slide"></img>
+                        <img className="d-block w-100 carousel-image" src={require('./img/Carousel/bootstrap.png')} alt="Third slide"></img>
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100 carousel-image" src={require('./img/Carousel/Java.png')} alt="Third slide"></img>
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100 carousel-image" src={require('./img/Carousel/react.png')} alt="Third slide"></img>
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100 carousel-image" src={require('./img/Carousel/git.png')} alt="Third slide"></img>
                     </div>
                 </div>
                 <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Previous</span>
+                    <span className="sr-only halfCircleone">Previous</span>
+                    <div className="half-left"></div>
                 </a>
                 <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Next</span>
+                    <span className="sr-only halfCircletwo">Next</span>
+                    <div className="half-right"></div>
                 </a>
+                <p className = "image-title">BLABLA</p>
             </div>
         )
     }
