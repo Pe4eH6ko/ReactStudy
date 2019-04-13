9b0bd5c7f23e17576d45645857ffe5e94d9c579e
Ivan <Poturnak.Ivan@gmail.com>
2019-04-13 23:00:26 +0300

    Make about me part

--- src/Aboutme.js
+++ src/Aboutme.js
@@ -1,47 +1,57 @@
 import React from 'react';
+import "./Aboutme.scss";
 
-function Aboutme (){
-    return(
-        <div>
-            <div>
-
-            </div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-3">
-                        <img></img>
-                        <h2>Head</h2>
-                        <h3>xxx</h3>
-                        <p></p>
-                        <p></p>
-                        <p></p>
+class Aboutme extends React.Component{
+    render(){
+        return(
+            <div className = "aboutPart">
+                <div className = "about-head">
+                    <h1 className="head">ABOUT ME</h1>
+                    <p>I AM A WEB DEVELOPER FROM FRANKIVSK</p>
+                </div>
+                <div className="container aboutText">
+                    <div className="row parts">
+                        <div className="col-md-3 about-column">
+                            <img className="about-image" src={require('./img/movies.png')} alt="movies"></img>
+                            <h2>MOVIES</h2>
+                            <h5>WHAT I WATCH</h5>
+                            <p>An American government organisation has 35 reports from 2010–2019. 
+                                These reports show that people have seizures after they use e-cigarettes.
+                                An American government organisation has 35 reports from 2010–2019. 
+                                These reports show that people have seizures after they use e-cigarettes.</p>
+                        </div>
+                        <div className="col-md-3 about-column">
+                            <img className="about-image" src={require('./img/music.png')} alt="music"></img>
+                            <h2>MUSIC</h2>
+                            <h5>WHAT I LISTEN</h5>
+                            <p>An American government organisation has 35 reports from 2010–2019. 
+                                These reports show that people have seizures after they use e-cigarettes.
+                                An American government organisation has 35 reports from 2010–2019. 
+                                These reports show that people have seizures after they use e-cigarettes.</p>
+                        </div>
+                        <div className="col-md-3 about-column">
+                            <img className="about-image" src={require('./img/clothes.png')} alt="clothes"></img>
+                            <h2>CLOTHES</h2>
+                            <h5>WHAT I WEAR</h5>
+                            <p>An American government organisation has 35 reports from 2010–2019. 
+                                These reports show that people have seizures after they use e-cigarettes.
+                                An American government organisation has 35 reports from 2010–2019. 
+                                These reports show that people have seizures after they use e-cigarettes.</p>
+                        </div>
+                        <div className="col-md-3 about-column">
+                            <img className="about-image" src={require('./img/food.png')} alt="food"></img>
+                            <h2>FOOD</h2>
+                            <h5>WHAT I EAT</h5>
+                            <p>An American government organisation has 35 reports from 2010–2019. 
+                                These reports show that people have seizures after they use e-cigarettes.
+                                An American government organisation has 35 reports from 2010–2019. 
+                                These reports show that people have seizures after they use e-cigarettes.</p>
+                        </div>                                                            
                     </div>
-                    <div className="col-md-3">
-                        <img></img>
-                        <h2>Head</h2>
-                        <h3>xxx</h3>
-                        <p></p>
-                        <p></p>
-                        <p></p>
-                    </div>
-                    <div className="col-md-3">
-                        <img></img>
-                        <h2>Head</h2>
-                        <h3>xxx</h3>
-                        <p></p>
-                        <p></p>
-                        <p></p>
-                    </div>
-                    <div className="col-md-3">
-                        <img></img>
-                        <h2>Head</h2>
-                        <h3>xxx</h3>
-                        <p></p>
-                        <p></p>
-                        <p></p>
-                    </div>                                                            
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+export default Aboutme;
