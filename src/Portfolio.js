0441c94a2f06607b06e80464b2bfdc50e480be44
Ivan Poturnak <Poturnak.Ivan@gmail.com>
2019-04-17 13:46:02 +0300

    make portfolio part

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './Portfolio.scss';
 
 class Portfolio extends React.Component{
+    // constructor(props){
+    //     super(props);
+
+    //     this.changeText = this.changeText.bind(this);
+    // }
+    // changeText() {
+    //     var element = React.findDOMNode(this.refs.seeAll);
+    //     console.log(element);
+    //     //.childNodes.innerText("Nothing yet =)")
+    // }
     render(){
         return(
             <div className="portfolioPart">
@@ -28,8 +38,7 @@ class Portfolio extends React.Component{
                     </div>
                     <div className="col-md-4 card workPage">
                         <a href="" className="worksHref">
-                            <div className="loader"></div>
-                            {/*<img src="" className="card-img-top worksImage" alt="..."></img>*/}
+                            <img src={require('./img/Pacman-1.gif')} className="card-img-top worksImage packman" alt="Coming soon"></img>
                             <div className="card-body worksName">
                                 <p className="card-text">COMING SOON</p>
                             </div>
@@ -37,8 +46,7 @@ class Portfolio extends React.Component{
                     </div>
                     <div className="col-md-4 card workPage">
                         <a href="" className="worksHref">
-                            <div className="loader"></div>
-                            {/*<img src="" className="card-img-top worksImage" alt="..."></img>*/}
+                            <img src={require('./img/Pacman-1.gif')} className="card-img-top worksImage packman" alt="Coming soon"></img>
                             <div className="card-body worksName">
                                 <p className="card-text">COMING SOON</p>
                             </div>
@@ -46,8 +54,7 @@ class Portfolio extends React.Component{
                     </div>
                     <div className="col-md-4 card workPage">
                         <a href="" className="worksHref">
-                            <div className="loader"></div>
-                            {/*<img src="" className="card-img-top worksImage" alt="..."></img>*/}
+                            <img src={require('./img/Pacman-1.gif')} className="card-img-top worksImage packman" alt="Coming soon"></img>
                             <div className="card-body worksName">
                                 <p className="card-text">COMING SOON</p>
                             </div>
@@ -55,15 +62,14 @@ class Portfolio extends React.Component{
                     </div>
                     <div className="col-md-4 card workPage">
                         <a href="" className="worksHref">
-                            <div className="loader"></div>
-                            {/*<img src="" className="card-img-top worksImage" alt="..."></img>*/}
+                            <img src={require('./img/Pacman-1.gif')} className="card-img-top worksImage packman" alt="Coming soon"></img>
                             <div className="card-body worksName">
                                 <p className="card-text">COMING SOON</p>
                             </div>
                         </a>
                     </div> 
                 </div>
-                <p>See all...</p>                                                                             
+                <a href="" id="seeAll" ref="seeAll" /*onClick={this.changeText()}*/><p>SEE ALL...</p></a>                                                                             
             </div>
         )
     }
