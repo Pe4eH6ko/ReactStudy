d2b3aa33ac6ecb7ac8cbadccbdb598b11c369fa6
Ivan <Poturnak.Ivan@gmail.com>
2019-04-29 18:56:30 +0300

    Change navbar file and delete NvBArElements

--- src/Navbar/NavBarElement.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default props => (
-    <li className={props.liClass}>
-        <a className={props.linkClass} href={props.href}>{props.text}</a>
-    </li>
-)
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,40 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Navbar.scss';
-import NavBarElement from './NavBarElement';
+
 
 class Navbar extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             elements: [
-                {text: 'ABOUT ME', href:'#aboutMe', key='1'},
-                {text: 'PORTFOLIO', href:'#portfolio', key='2'},
-                {text: 'BLOG', href:'#blog', key='3'},
-                {text: 'CONTACT ME', href:'#contactme', key='4'}          
-            ],
-            linkClass: 'nav-link element',
-            liClass: 'justify-content-center nav-item'
+                {text: 'ABOUT ME', href:'#aboutMe', key:'1', linkClass: 'nav-link element', liClass: 'justify-content-center nav-item'},
+                {text: 'PORTFOLIO', href:'#portfolio', key:'2', linkClass: 'nav-link element', liClass: 'justify-content-center nav-item'},
+                {text: 'BLOG', href:'#blog', key:'3', linkClass: 'nav-link element', liClass: 'justify-content-center nav-item'},
+                {text: 'CONTACT ME', href:'#contactme', key:'4', linkClass: 'nav-link element', liClass: 'justify-content-center nav-item'}          
+            ]  
         };
+    }
+    componentDidMount(){
+        this.setState({elements: this.state.elements});
     }
 render() {
         return (
             <nav className ="navbar fixed-top navbar-expand-lg justify-content-center myMenu">
                 <div className="justify-content-center">               
                      <ul className="justify-content-center navbar-nav menuBlock" id="menu">
-                    
-                        {/* <li className="justify-content-center nav-item">
-                            <a className="nav-link element" href="#" onClick={this.makeMenu}>ABOUT ME</a>
-                        </li>
-                        <li className="justify-content-center nav-item">
-                            <a className="nav-link element" href="#">PORTFOLIO</a>
-                        </li>
-                        <li className="justify-content-center nav-item">
-                            <a className="nav-link element" href="#">MY BLOG</a>
-                        </li>
-                        <li className="justify-content-center nav-item ">
-                            <a className="nav-link element" href="#">CONTACT ME</a>
-                        </li>  */}
+                        {this.state.elements.map((li, i) => 
+                            <li className = {li.liClass} key={i}>
+                                <a className = {li.linkClass} href={li.href} key={i}>{li.text}</a>    
+                            </li>)}
                     </ul> 
                 </div>
             </nav>
