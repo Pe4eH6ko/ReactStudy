5a1ee4e1cd44d74f03daad547650f5f4bb7c8b9a
Ivan Poturnak <Poturnak.Ivan@gmail.com>
2019-05-03 14:28:00 +0300

    make aboutme-makeTextMethod

--- src/Aboutme/Aboutme.js
+++ src/Aboutme/Aboutme.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import "./Aboutme.scss";
+import $ from 'jquery';
+
 
 class Aboutme extends React.Component{
+    constructor(props){
+        super(props); 
+        this.makeText = this.makeText.bind(this);
+        this.state = {
+            elements:[
+                {divClass:'col-md-3 about-column', imgClass:'about-image', src:require('./img/movies.png'), alt:'movies', text:'"./text/movies.txt"'},
+                {divClass:'col-md-3 about-column', imgClass:'about-image', src:require('./img/music.png'), alt:'music', text:'"./text/music.txt"'},
+                {divClass:'col-md-3 about-column', imgClass:'about-image', src:require('./img/clothes.png'), alt:'clothes', text:'"./text/movies.txt"'},
+                {divClass:'col-md-3 about-column', imgClass:'about-image', src:require('./img/food.png'), alt:'food', text:'"./text/movies.txt"'}
+            ]
+        }  
+    }
+    componentDidMount(){
+        this.setState({elements: this.state.elements});
+    }
+    makeText(fileAdress){
+          
+    }
     render(){
         return(
             <div className = "aboutPart">
@@ -11,16 +31,15 @@ class Aboutme extends React.Component{
                 </div>
                 <div className="container aboutText">
                     <div className="row parts">
-                        <div className="col-md-3 about-column">
-                            <img className="about-image" src={require('./img/movies.png')} alt="movies"></img>
-                            <h2>MOVIES</h2>
-                            <h5>WHAT I WATCH</h5>
-                            <p>An American government organisation has 35 reports from 2010–2019. 
-                                These reports show that people have seizures after they use e-cigarettes.
-                                An American government organisation has 35 reports from 2010–2019. 
-                                These reports show that people have seizures after they use e-cigarettes.</p>
-                        </div>
-                        <div className="col-md-3 about-column">
+                        {this.state.elements.map((div,i) =>
+                            <div className={div.divClass} key={i}>
+                                <img className={div.imgClass} src={div.src} alt={div.alt}></img>
+                                <h2>MOVIES</h2>
+                                <h5>WHAT I WATCH</h5>
+                                <p className='text'>{this.makeText(div.text)}</p> 
+                            </div>
+                        )}
+                        {/* <div className="col-md-3 about-column">
                             <img className="about-image" src={require('./img/music.png')} alt="music"></img>
                             <h2>MUSIC</h2>
                             <h5>WHAT I LISTEN</h5>
@@ -46,7 +65,7 @@ class Aboutme extends React.Component{
                                 These reports show that people have seizures after they use e-cigarettes.
                                 An American government organisation has 35 reports from 2010–2019. 
                                 These reports show that people have seizures after they use e-cigarettes.</p>
-                        </div>                                                            
+                        </div>                                                             */}
                     </div>
                 </div>
             </div>
