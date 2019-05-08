e49cb382c243a15eaf681106ad7580d9f27cca1a
Ivan <Poturnak.Ivan@gmail.com>
2019-05-08 23:04:39 +0300

    commit 2304 08052019

--- src/Aboutme/Aboutme.js
+++ src/Aboutme/Aboutme.js
@@ -9,25 +9,31 @@ class Aboutme extends React.Component{
         this.makeText = this.makeText.bind(this);
         this.state = {
             elements:[
-                {divClass:'col-md-3 about-column', imgClass:'about-image', src:require('./img/movies.png'), alt:'movies', text:'text/movies.txt', word:'WATCH'},
-                {divClass:'col-md-3 about-column', imgClass:'about-image', src:require('./img/music.png'), alt:'music', text:'text/music.txt', word:'LISTEN'},
-                {divClass:'col-md-3 about-column', imgClass:'about-image', src:require('./img/clothes.png'), alt:'clothes', text:'text/clothes.txt', word:'WEAR'},
-                {divClass:'col-md-3 about-column', imgClass:'about-image', src:require('./img/food.png'), alt:'food', text:'text/food.txt', word:'EAT'}
+                {src:require('./img/movies.png'), alt:'movies', text: require('./text/movies.txt'), word:'WATCH'},
+                {src:require('./img/music.png'), alt:'music', text:require('./text/music.txt'), word:'LISTEN'},
+                {src:require('./img/clothes.png'), alt:'clothes', text:require('./text/clothes.txt'), word:'WEAR'},
+                {src:require('./img/food.png'), alt:'food', text:require('./text/food.txt'), word:'EAT'}
             ]
         }  
     }
     componentDidMount(){
         this.setState({elements: this.state.elements});
+        window.addEventListener('load', this.makeText);
     }
-    makeText(fileAdress){
-        $.ajax({
-            url: fileAdress,
-            dataType:"text",
-            success: function(data){
-                console.log(data);
-            }
-
-        });
+    makeText(){
+        var text;
+        var paragraf = document.getElementsByClassName('text');
+        const story = this.state.elements;
+        story.map((elements, j) =>
+            $.ajax({
+                url: elements.text,
+                dataType:"text",
+                success: function(data){
+                    text = document.createTextNode(data);
+                    paragraf[j].appendChild(text); 
+                }
+            })
+        )
     }
     render(){
         return(
@@ -39,11 +45,11 @@ class Aboutme extends React.Component{
                 <div className="container aboutText">
                     <div className="row parts">
                         {this.state.elements.map((div,i) =>
-                            <div className={div.divClass} key={i}>
-                                <img className={div.imgClass} src={div.src} alt={div.alt}></img>
+                            <div className='col-md-3 about-column' key={i}>
+                                <img className='about-image' src={div.src} alt={div.alt}></img>
                                 <h2>{div.alt.toUpperCase()}</h2>
                                 <h5>WHAT I {div.word}</h5>
-                                <p className='text'>{this.makeText(div.text)}</p> 
+                                <p className='text'></p> 
                             </div>
                         )}                                                             
                     </div>
